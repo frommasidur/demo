aa2dd1fa7361a0fa04430e4e86b02122ca48f2bc
Masidur Rahaman <mrahaman@maxlinear.com>
2023-09-21 08:39:10 +0530

    button added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import logo from './logo.svg';
+import logo1 from './logo.svg';
 import './App.css';
+
+function MyButton() {
+  return (
+    <button>Click here</button>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo1} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit save to reload Masidur.
         </p>
         <a
           className="App-link"
@@ -17,6 +23,7 @@ function App() {
         >
           Learn React
         </a>
+        < MyButton />
       </header>
     </div>
   );
